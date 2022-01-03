99ac42c57a58b8f6563b62af3aad88eb5d45667a
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2022-01-03 21:13:19 +0100

    Initial commit

---
+++ desktop/src/com/dontstopshooting/dontstopshooting/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dontstopshooting.dontstopshooting.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dontstopshooting.dontstopshooting.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
